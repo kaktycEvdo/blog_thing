aefbfdbf8b76600ebcec41bee6570b9785ee7889
kaktycEvdo <ilya.tretyakov.2005@bk.ru>
2024-10-15 18:23:41 +0700

    site init

---
+++ blog-project/view/components/leftpanel.php
@@ -0,0 +1,32 @@
+<section id="left-panel">
+    <div class="profile_left">
+    <!-- Profile's media pictures -->
+        <div class="profile_media">
+            <div class="background">
+                <img src="" alt="<?php echo "Profile's bg" ?>">
+            </div>
+            <div class="pfp">
+                <img src="" alt="<?php echo "Profile's name" ?>">
+            </div>
+        </div>
+        <div class="brief">
+            <!-- Profile's name and description -->
+            <p>Андрей</p>
+            <p>Краткое описание</p>
+            <div class="contacts">
+                <a href="custom link"><img src="" alt="Twitter link"></a>
+                <a href="custom link"><img src="" alt="VK link"></a>
+                <a href="custom link"><img src="" alt="Pinterest link"></a>
+            </div>
+        </div>
+        <hr>
+        <div class="big_desc">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque nesciunt quis perspiciatis voluptate temporibus fuga officia vero non consectetur. Voluptatibus?
+        </div>
+        <hr>
+        <div class="profile_left_buttons">
+            <a href="profilelink/works" class="works">Мои работы</a>
+            <a href="profilelink/message" class="message">Написать мне</a>
+        </div>
+    </div>
+</section>
